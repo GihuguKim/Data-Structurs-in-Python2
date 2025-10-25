95bcd10f5e7684646b53e701547493419ddcfc9a
GihuguKim <kimgyannag1@gmail.com>
2025-10-25 18:46:19 +0200

    Add files via upload

---
+++ OperationsinUnionandIntersection.py
@@ -0,0 +1,11 @@
+setc1 = {"green", "blue"}
+setc2 = {"blue", "yellow"}
+print("original sets")
+print(setc1)
+print(setc2)
+setc = setc1.intersection(setc2)
+print("Intersection of setc1 and setc2 is : ")
+print(setc)
+setu = setc1.union(setc2)   
+print("Union of setc1 and setc2 is : ")
+print(setu)
